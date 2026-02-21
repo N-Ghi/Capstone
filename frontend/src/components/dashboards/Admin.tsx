9c76500aa167cccd204f76a7ae42d4036d471a76
N-Ghi <g.nagasaro@alustudent.com>
2026-02-21 20:11:08 +0200

    feat: add dashboard components for user roles
    - Add Admin dashboard
    - Add Guide dashboard
    - Add Tourist dashboard

---
+++ frontend/src/components/dashboards/Admin.tsx
@@ -0,0 +1,14 @@
+/**
+ * Admin dashboard component; provides an overview of the application and its features,
+
+ */
+import React from 'react';
+
+
+const AdminDashboard: React.FC = () => {
+    return (
+        <p>Admin Dashboard</p>
+    );
+};
+
+export default AdminDashboard;
---
+++ frontend/src/components/dashboards/Tourist.tsx
@@ -0,0 +1,14 @@
+/**
+ * Tourist dashboard component; provides an overview of the application and its features,
+
+ */
+import React from 'react';
+
+
+const TouristDashboard: React.FC = () => {
+    return (
+        <p>Tourist Dashboard</p>
+    );
+};
+
+export default TouristDashboard;
