05fa710dabc0129a4a7ef5d58636dd2dad020b6f
N-Ghi <g.nagasaro@alustudent.com>
2026-02-27 12:05:36 +0200

    feat: enhance user authentication flow and add Google Calendar linking

--- frontend/src/components/common/Icons.tsx
+++ frontend/src/components/common/Icons.tsx
@@ -1,3 +1,27 @@
-import { Pencil, Trash2, Eye, MapPin, Calendar, Images, ImageOff, Plus, ChevronLeft, AlertCircle, ChevronDown, Check, Home, X, BookCheckIcon} from 'lucide-react';
+import { 
+  Pencil, Trash2, Eye, MapPin, Calendar, Images, ImageOff, Plus, 
+  ChevronLeft, AlertCircle, ChevronDown, Check, Home, X, 
+  BookCheckIcon, Link, ChevronRight, KeyRound, Loader2 
+} from 'lucide-react';
 
-export { Pencil as EditIcon, Trash2 as DeleteIcon, Eye as ViewIcon, MapPin as LocationIcon, Calendar  as DateIcon, Images as PhotoCountIcon, ImageOff as NoPhotoIcon, Plus as AddIcon, ChevronLeft as BackIcon, AlertCircle as ErrorIcon, ChevronDown as DropdownIcon, Check as CheckIcon, Home as HomeIcon, X as CloseIcon, BookCheckIcon as BookIcon };
+export { 
+  Pencil as EditIcon,
+  Trash2 as DeleteIcon,
+  Eye as ViewIcon,
+  MapPin as LocationIcon,
+  Calendar as DateIcon,
+  Images as PhotoCountIcon,
+  ImageOff as NoPhotoIcon,
+  Plus as AddIcon,
+  ChevronLeft as BackIcon,
+  AlertCircle as ErrorIcon,
+  ChevronDown as DropdownIcon,
+  Check as CheckIcon,
+  Home as HomeIcon,
+  X as CloseIcon,
+  BookCheckIcon as BookIcon,
+  Link as LinkIcon,
+  ChevronRight as NextIcon,
+  KeyRound as KeyIcon,
+  Loader2 as LoaderIcon
+};
--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -1,4 +1,6 @@
-import { Navigate } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
+import { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import type { Role } from "../@types/auth.types";
 import type { JSX } from "react/jsx-dev-runtime";
@@ -9,18 +11,28 @@ interface Props {
 }
 
 const ProtectedRoute = ({ children, allowedRoles }: Props) => {
+  const { t } = useTranslation('auth');
   const { user, loading } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      navigate("/login", {
+        replace: true,
+        state: {
+          message: t('login.required'),
+          from: location.pathname + location.search,
+        },
+      });
+    }
+  }, [user, loading, navigate, location.pathname]);
 
   if (loading) return <div>Loading...</div>;
+  if (!user) return null;
 
-  // Not logged in
-  if (!user) {
-    return <Navigate to="/login" replace />;
-  }
-
-  // Role restriction
   if (allowedRoles && !allowedRoles.includes(user.role)) {
-    return <Navigate to="/" replace />;
+    return null;
   }
 
   return children;
